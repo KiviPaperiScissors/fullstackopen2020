41ff1550942321556db082c24ccbe9716b29370d
Vilhelm Lindholm <vilhelm.lindholm@utu.fi>
2020-06-29 15:15:44 +0300

    part 2 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import Persons from './Components/Person'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
+import Notifications from './Components/Notification'
+import personService from './services/persons'
 
 
 const App = () => {
@@ -10,14 +12,14 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ phonebookFilterString, setPhonebookFilterString ] = useState('')
+  const [ notificationMessage, setNotificationMessage ] = useState(null)
+  const [ errorMessage, setErrorMessage ] = useState(null)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -25,19 +27,97 @@ const App = () => {
     event.preventDefault()
 
     if (persons.find(person => person.name === newName)) {
-      window.alert(`${newName} is already added to phonebook`)
-      return
-    }
+      let updateNumber =
+        window.confirm(`${newName} is already added to phonebook.
+          Would you like to update the number to ${newNumber}?`)
+      if (updateNumber) {
+        let id = persons.find(person => person.name === newName).id
+        console.log('attempting to update number')
+        const personObject = {
+          name: newName,
+          number: newNumber
+        }
+
+        personService
+          .update(id, personObject)
+
+          .then(updatedPerson =>{
+            console.log('update promise succesful')
+            setPersons(persons.map(person => person.id !== id
+              ? person : updatedPerson))
+            setNotificationMessage (
+              `${personObject.name}'s number was changed to ${personObject.number}.`
+            )
+          })
+
+          .catch(error => {
+            console.log('error caught')
+            setErrorMessage(
+              `That person was already removed from the server.`
+            )
+            personService.getAll()
+              .then(directory =>
+                setPersons(directory))
+            console.log('persons:', persons)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+
+          })
+
+          setNewName('')
+          setNewNumber('')
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 5000)
+
+          return
+        } else {
+        console.log('update cancelled')
+        return
+        }
+      }
+
 
     const personObject = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNotificationMessage(`${returnedPerson.name} was added to the directory.`)
+        setNewName('')
+        setNewNumber('')
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000)
+
+      })
+    }
+
+
+  const deletePerson = (event, id) => {
+    let areYouSure = window
+      .confirm(`Are you sure you want to remove ${persons.find(p => p.id === id).name}?`)
+
+    if (areYouSure) {
+      let delPersName = persons.find(p => p.id === id).name
+      console.log(`Attempting to delete entry ${id}`)
+      personService
+        .removePerson(id)
+        .then(response => {
+          setPersons(persons.filter(p => p.id !== id))
+        })
+        setNotificationMessage(`${delPersName} deleted from the directory.`)
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000)
+    } else console.log('He chickened out, sir!')
   }
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -58,6 +138,9 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
 
+      <Notifications.Notification message={notificationMessage} />
+      <Notifications.Error message={errorMessage} />
+
       <Filter phonebookFilterString={phonebookFilterString} handleFilterChange={handleFilterChange} />
 
       <h2>Add new entries</h2>
@@ -66,7 +149,7 @@ const App = () => {
 
       <h2>Entries</h2>
 
-      <Persons persons={persons} phonebookFilterString={phonebookFilterString} />
+      <Persons persons={persons} phonebookFilterString={phonebookFilterString} deletePerson={deletePerson}/>
 
     </div>
   )
--- part2/phonebook/src/Components/Person.js
+++ part2/phonebook/src/Components/Person.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Person = ({ person }) => (
-  <li> {person.name} {person.number}</li>
+const Person = (props) => (
+  <li>
+    {props.person.name} {props.person.number}
+    <button onClick={(e) => props.deletePerson(e, props.person.id)}>
+      Delete </button>
+  </li>
 )
 
 const Persons = (props) => (
   <ul>
     {props.persons.filter(person =>
       person.name.toUpperCase().includes(props.phonebookFilterString.toUpperCase())).map(filteredPerson => (
-        <Person key={filteredPerson.name} person={filteredPerson} />
+        <Person key={filteredPerson.id} person={filteredPerson}
+        deletePerson = {props.deletePerson}/>
     ))}
   </ul>
 )
